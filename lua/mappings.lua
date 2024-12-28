d66a3298d7444c25d9190afbb40d59f23acb8f73
minhquan186 <lmquan.1860@gmail.com>
2024-12-28 15:37:44 +0700

    Initial commit

---
+++ lua/mappings.lua
@@ -0,0 +1,32 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+local map = vim.keymap
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+map.set('n', '<C-h>', '<C-w>h', opts)
+map.set('n', '<C-j>', '<C-w>j', opts)
+map.set('n', '<C-k>', '<C-w>k', opts)
+map.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+map.set('n', '<C-Up>', ':resize -2<CR>', opts)
+map.set('n', '<C-Down>', ':resize +2<CR>', opts)
+map.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+map.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-----------------
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+map.set('v', '<', '<gv', opts)
+map.set('v', '>', '>gv', opts)
---
+++ lua/options.lua
@@ -0,0 +1,26 @@
+local o = vim.opt
+
+o.clipboard = 'unnamedplus'   -- use system clipboard 
+o.completeopt = {'menu', 'menuone', 'noselect'}
+o.mouse = 'a'                 -- allow the mouse to be used in Nvim
+
+-- Tab
+o.tabstop = 4                 -- number of visual spaces per TAB
+o.softtabstop = 4             -- number of spacesin tab when editing
+o.shiftwidth = 4              -- insert 4 spaces on a tab
+o.expandtab = true            -- tabs are spaces, mainly because of python
+
+-- UI config
+o.number = true               -- show absolute number
+o.relativenumber = true       -- add numbers to each line on the left side
+o.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+o.splitbelow = true           -- open new vertical split bottom
+o.splitright = true           -- open new horizontal splits right
+-- o.termguicolors = true        -- enabl 24-bit RGB color in the TUI
+o.showmode = false            -- we are experienced, wo don't need the "-- INSERT --" mode hint
+
+-- Searching
+o.incsearch = true            -- search as characters are entered
+o.hlsearch = false            -- do not highlight matches
+o.ignorecase = true           -- ignore case in searches by default
+o.smartcase = true            -- but make it case sensitive if an uppercase is entered
