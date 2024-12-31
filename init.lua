e9f05eaeab955513fc437ba985b98e42b50bdc15
minhquan186 <91863949+minhquan186@users.noreply.github.com>
2024-12-31 11:02:26 +0700

    Add more keymapping + Code completion

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require('core.options')
 require('core.mappings')
 require('core.lazy')
+require('core.autocmds')
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,28 +6,60 @@ local opts = {
 vim.g.mapleader = " "
 local map = vim.keymap
 
+----------------------
+-- Universal keymap --
+----------------------
+
+-- Line wrapping
+map.set({ "n", "i", "v" }, "<A-z>", function()
+    vim.cmd("set wrap!")
+end, { desc = "Toggle linewrapping" })
+
+-- NvimTree
+map.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+
 -----------------
 -- Normal mode --
 -----------------
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-map.set('n', '<C-h>', '<C-w>h', opts)
-map.set('n', '<C-j>', '<C-w>j', opts)
-map.set('n', '<C-k>', '<C-w>k', opts)
-map.set('n', '<C-l>', '<C-w>l', opts)
+map.set("n", "<C-h>", "<C-w>h", opts)
+map.set("n", "<C-j>", "<C-w>j", opts)
+map.set("n", "<C-k>", "<C-w>k", opts)
+map.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 -- delta: 2 lines
-map.set('n', '<C-Up>', ':resize -2<CR>', opts)
-map.set('n', '<C-Down>', ':resize +2<CR>', opts)
-map.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-map.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+map.set("n", "<C-Up>", ":resize -2<CR>", opts)
+map.set("n", "<C-Down>", ":resize +2<CR>", opts)
+map.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+map.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Window management
+map.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+map.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+map.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- Tab management
+map.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open New Tab" })
+map.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+map.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+map.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to prev tab" })
+map.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Line number
+map.set("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+map.set("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+
+-- nvimtree
+map.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
 
 -----------------
 -- Visual mode --
 -----------------
 
 -- Hint: start visual mode with the same area as the previous area and the same mode
-map.set('v', '<', '<gv', opts)
-map.set('v', '>', '>gv', opts)
+map.set("v", "<", "<gv", opts)
+map.set("v", ">", ">gv", opts)
