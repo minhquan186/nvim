975e994ca4bc30f5a00d6f72b87a1975a23ac379
minhquan186 <lmquan.1860@gmail.com>
2025-01-01 10:15:43 +0700

    Config telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,10 +5,29 @@ return {
       'nvim-lua/plenary.nvim'
     },
     config = function()
+      -- Keymapping
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      -- Telescope setup
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      -- Action setup
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+              ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+      })
+      -- Telescope extensions
+      telescope.load_extension("fzf")
     end
 }
