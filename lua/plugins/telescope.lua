fd6ae34f1add96d822f15b7a09de1173e1517ff9
minhquan186 <lmquan.1860@gmail.com>
2025-01-01 10:19:19 +0700

    Fix telescope fzf not found

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,21 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim", -- FZF
+        build = "make"
+      },
+      "nvim-tree/nvim-web-devicons", -- dev icons
     },
     config = function()
       -- Keymapping
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
       -- Telescope setup
       local telescope = require("telescope")
       local actions = require("telescope.actions")
