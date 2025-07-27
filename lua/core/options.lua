9b548cda78faf2882b7de9b71501ed0463ae70e7
minhquan186 <lmquan.1860@gmail.com>
2025-07-27 11:08:04 +0700

    Update shell detection via OS

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -38,16 +38,24 @@ o.smartcase = true            -- but make it case sensitive if an uppercase is e
 ------------------
 -- Shell config --
 ------------------
-o.shell = 'pwsh'
-local powershell_options = {
-  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
+local is_windows = vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16")
+if is_windows then
+  local hasWSL = vim.fn.has("wsl")
+  if not hasWSL then
+    o.shell = 'pwsh'
+    local powershell_options = {
+      shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+      shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+      shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+      shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+      shellquote = "",
+      shellxquote = "",
+    }
 
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
+    for option, value in pairs(powershell_options) do
+      vim.opt[option] = value
+    end
+  end
+else
+  o.shell = "bash"
 end
